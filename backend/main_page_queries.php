d7a0f1935e008c4f1ba94a7416b9269a842d5b1d
Alex Yang <ayang40@students.cs.ubc.ca>
2025-04-02 10:32:55 -0700

    aggregation with HAVING query, donation page

--- backend/main_page_queries.php
+++ backend/main_page_queries.php
@@ -2,26 +2,27 @@
 include("connection.php");
 include("db_utils.php");
 
-function handleResetRequest()
-{
-    global $db_conn;
-    echo "<br> creating new disaster relief tables...<br>";
-    executeSQLFile('../disasterrelief.sql');
-}
+// function handleResetRequest()
+// {
+//     global $db_conn;
+//     echo "<br> creating new disaster relief tables...<br>";
+//     executeSQLFile('../disasterrelief.sql');
+// }
 
-function handleCountRequest()
-{
-    global $db_conn;
+// function handleCountRequest()
+// {
+//     global $db_conn;
 
-    $result = executePlainSQL("SELECT Count(*) FROM Disaster");
+//     $result = executePlainSQL("SELECT Count(*) FROM Disaster");
 
-    if (($row = oci_fetch_row($result)) != false) {
-        echo "<br> The number of tuples in Disaster: " . $row[0] . "<br>";
-    }
-}
+//     if (($row = oci_fetch_row($result)) != false) {
+//         echo "<br> The number of tuples in Disaster: " . $row[0] . "<br>";
+//     }
+// }
 
 // TODO: modify the SELECT clause to include user-input params,
 // if user hasn't input any params, then select all (*)
+// SELECTION query
 function handleDisasterDisplayRequest()
 {
     global $db_conn;
@@ -39,6 +40,7 @@ function handleDisasterDisplayRequest()
         "type" => "type"
     ];
 
+    // might need to split up the queries because of date etc.
     foreach ($filters as $column => $param) {
         if (!empty($_GET[$param])) {
             $query .= " AND LOWER($column) LIKE LOWER(:$param)";
@@ -78,7 +80,10 @@ function handleMissionDisplayRequest()
 
 function printResult($result)
 {
-    echo "<br>Retrieved data from table Disaster:<br>";
+    // echo "<br>Retrieved data from table Disaster:<br>";
+    // echo "<script>
+    //     document.getElementById('disaster_results').innerHTML = '';
+    // </script>";
     echo "<table>";
     $first_row = true;
     while ($row = OCI_Fetch_Array($result, OCI_ASSOC)) {
@@ -104,7 +109,93 @@ function printResult($result)
 
 function handleDisasterReliefProgressDisplayRequest()
 {
-    //TODO
+    global $db_conn;
+    // should probably be 2 subqueries
+    // one for sum supplies and one for sum donations
+
+    // TODO: tuples used in the query cannot be predetermined
+    // can use user input?
+    $query = "SELECT 
+        m.disasterName,
+        m.disasterLocation,
+        m.disasterDate,
+        m.helpNeeded,
+        m.priority,
+        SUM(s.quantity) AS totalSupplies,
+        SUM(d.donationAmount) AS totalDonations
+    FROM 
+        Mission m
+    JOIN 
+        ReliefCenter rc ON m.rcName = rc.name AND m.rcLocation = rc.location
+    LEFT JOIN 
+        Supplies s ON rc.name = s.rcName AND rc.location = s.rcLocation
+    LEFT JOIN 
+        Donation d ON rc.name = d.rcName AND rc.location = d.rcLocation
+    GROUP BY 
+        m.disasterName,
+        m.disasterLocation,
+        m.disasterDate,
+        m.helpNeeded,
+        m.priority
+    ORDER BY 
+        m.priority DESC";
+
+    $result = oci_parse($db_conn, $query);
+
+    oci_execute($result);
+
+    echo "<table border='1'>
+        <tr>
+            <th>Disaster Name</th>
+            <th>Location</th>
+            <th>Date</th>
+            <th>Help Needed</th>
+            <th>Priority</th>
+            <th>Total Supplies</th>
+            <th>Total Donations</th>
+        </tr>";
+
+    while ($row = oci_fetch_array($result, OCI_ASSOC+OCI_RETURN_NULLS)) {
+        echo "<tr>";
+        echo "<td>" . $row['DISASTERNAME'] . "</td>";
+        echo "<td>" . $row['DISASTERLOCATION'] . "</td>";
+        echo "<td>" . $row['DISASTERDATE'] . "</td>";
+        echo "<td>" . $row['HELPNEEDED'] . "</td>";
+        echo "<td>" . $row['PRIORITY'] . "</td>";
+        echo "<td>" . $row['TOTALSUPPLIES'] ?? 0 . "</td>";
+        echo "<td>" . $row['TOTALDONATIONS'] ?? 0 . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+
+    // oci_free_statement($result);  
+
+}
+
+// aggregation with HAVING
+function handleReliefCenterDonationDisplayRequest() 
+{
+    global $db_conn;
+
+    $amount = isset($_GET['donationAmount']) && is_numeric($_GET['donationAmount']) ? $_GET['donationAmount'] : PHP_INT_MAX;
+    $query = "SELECT rcName, rcLocation, SUM(donationAmount) as total_donations
+              FROM Donation
+              GROUP BY rcName, rcLocation
+              HAVING SUM(donationAmount) < :amount";
+
+    $result = oci_parse($db_conn, $query);
+    oci_bind_by_name($result, ":amount", $amount);
+    oci_execute($result);
+
+    echo "<table border='1'><tr><th>Relief Center Name</th><th>Location</th><th>Total Donations</th></tr>";
+
+    while ($row = oci_fetch_assoc($result)) {
+        echo "<tr><td>" . $row["RCNAME"] . "</td>";
+        echo "<td>" . $row["RCLOCATION"] . "</td>";
+        echo "<td>" . $row["TOTAL_DONATIONS"] . "</td></tr>";
+    }
+
+    echo "</table>";
 }
 
 function handleInsertRequest()
@@ -160,8 +251,11 @@ function handleRequest()
             handleDisasterDisplayRequest();
         } else if (array_key_exists('displayMissionTuples', $_GET)) {
             handleMissionDisplayRequest();
-        } else if (array_key_exists('displayDisasterReliefProgress')) {
+        } else if (array_key_exists('displayDisasterReliefProgressRequest', $_GET)) {
+            echo "hit";
             handleDisasterReliefProgressDisplayRequest();
+        } else if (array_key_exists('displayReliefCenterDonationRequest', $_GET)) {
+            handleReliefCenterDonationDisplayRequest();
         }
 
         disconnectFromDB();
--- frontend/pages/index.php
+++ frontend/pages/index.php
@@ -2,54 +2,14 @@
 <html>
 <head>
 	<title>Disaster Relief Project</title>
-	<link rel="stylesheet" href="../styles/index.css" type="text/css"/>
+	<link rel="stylesheet" href="../styles/index.css"/>
 </head>
 <body>
 	<!-- TODO: only shows tuples from Disaster; no inserts or updates yet -->
 	<div class="index_container">
-		<h2>Reset All Disaster Relief Tables</h2>
-		<p>If you wish to reset the table press on the reset button. If this is the first time you're running this page, you MUST use reset</p>
-
-		<form method="POST" action="">
-			<!-- "action" specifies the file or page that will receive the form data for processing. As with this example, it can be this same file. -->
-			<input type="hidden" id="resetTablesRequest" name="resetTablesRequest">
-			<p><input type="submit" value="Reset" name="reset"></p>
-		</form>
-
-		<hr />
-
-		<h2>Insert Values into ??</h2>
-		<form method="POST" action="">
-			<input type="hidden" id="insertQueryRequest" name="insertQueryRequest">
-			Number: <input type="text" name="insNo"> <br /><br />
-			Name: <input type="text" name="insName"> <br /><br />
-			<input type="submit" value="Insert" name="insertSubmit"></p>
-		</form>
-
-		<hr />
-
-		<h2>Update Name in ??</h2>
-		<p>The values are case sensitive and if you enter in the wrong case, the update statement will not do anything.</p>
-
-		<form method="POST" action="">
-			<input type="hidden" id="updateQueryRequest" name="updateQueryRequest">
-			Old Name: <input type="text" name="oldName"> <br /><br />
-			New Name: <input type="text" name="newName"> <br /><br />
-
-			<input type="submit" value="Update" name="updateSubmit"></p>
-		</form>
-
-		<hr />
-
-		<h2>Count the Tuples in Disaster</h2>
-		<form method="GET" action="">
-			<input type="hidden" id="countTupleRequest" name="countTupleRequest">
-			<input type="submit" name="countTuples"></p>
-		</form>
-
-
-		<hr />
-
+	
+	<div class="disaster_container" style="display: flex; flex-direction: row;">
+	<div class="disaster_form" style="display: flex; flex-direction: column; justify-content: center; padding-left: 1rem;">
 		<h2>View Disasters</h2>
 		<form method="GET" action="">
 			<label for="disasterName">Disaster Name:</label>
@@ -84,6 +44,22 @@
 			<input type="hidden" id="displayDisasterTuplesRequest" name="displayDisasterTuplesRequest">
 			<input type="submit" name="displayDisasterTuples"></p>
 		</form>
+	</div>
+	<div class="disaster_results_container">
+		<h2>Disaster Results</h2>
+		<div id="disaster_results">
+			<?php
+			// display results of the disaster query in container
+			if (isset($_GET['displayDisasterTuples'])) {
+				include("../../backend/main_page_queries.php");
+				handleDisasterDisplayRequest();
+			}
+			?>
+		</div>
+
+	</div>
+	</div>
+
 
 		<hr />
 
@@ -128,6 +104,37 @@
 			<input type="submit" name="displayDisasterReliefProgress"></p>
 		</form>
 
+	</div>
+	<div>
+		<h2 style="display: inline;">Find Relief Centers with less than </h2> 
+		<form method="GET" action="" id="donationForm" style="display: inline;">
+			<input type="number" id="donationAmount" name="donationAmount">
+			<input type="hidden" id="displayReliefCenterDonationRequest" name="displayReliefCenterDonationRequest">
+		</form>
+		<h2 style="display: inline;">in donations</h2>
+		<input type="submit" name="displayReliefCenterDonation" form="donationForm" value="Search"></p>
+		<h4>(leave input blank to see all relief centers)</h4>
+	</div>
+
+	<div>
+		<h2>Donate to help our cause!</h2>
+		<button onclick="window.location.href='donationPage.php'">Donate Here</button>
+		<!-- <form method="POST" action="donationPage">
+			<input type="hidden" id="donationRequest" name="donationRequest">
+			<label for="donorName">Donor Name:</label>
+			<input type="text" id="donorName" name="donorName"><br><br>
+
+			<label for="donationAmount">Donation Amount:</label>
+			<input type="number" id="donationAmount" name="donationAmount"><br><br>
+
+			<label for="rcName">Relief Center Name:</label>
+			<input type="text" id="rcName" name="rcName"><br><br>
+
+			<label for="rcLocation">Relief Center Location:</label>
+			<input type="text" id="rcLocation" name="rcLocation"><br><br>
+
+			<input type="submit" value="Donate"></p>
+		</form> -->
 	</div>
 </body>
 </html>
